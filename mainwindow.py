ed9179a8bb80cc91184a113afa4cc5b4880b8b70
vaca22 <wsmslgh@gmail.com>
2024-01-08 17:51:06 +0800

    sdfgsdfgdsffdgffgdfghghffdgghjhhfddguighjfghfgh

--- mainwindow.py
+++ mainwindow.py
@@ -255,7 +255,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "订单导出助手"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "订单导出助手 v2.3"))
 
 
     def logoutClick(self):
