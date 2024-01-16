ffb64afc9432db05d5e2e2d7b35509307dff0a67
vaca22 <wsmslgh@gmail.com>
2024-01-16 13:56:40 +0800

    change to http://39.101.183.9:1818/sph_portal

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ from qr_info_class import qr_info_class
 from user_class import UserInfo
 
 # Define the base URL
-base_url = "http://18.162.149.95/sph_portal/user/"
+base_url = "http://39.101.183.9:1818/sph_portal/user/"
 # base_url = "http://1.14.135.210:8569/sph_portal/user/"
 
 
